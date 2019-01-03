944a10b6c0cbc705ddc48efc39bc792fd0194911
Damian Cugley <pdc@alleged.org.uk>
2019-01-03 22:21:58 +0000

    Correct method name in admin module.

--- tanolin/images/admin.py
+++ tanolin/images/admin.py
@@ -8,7 +8,7 @@ from .models import Image
 def queue_retrieve(model_admin, request, queryset):
     """Queue the images to be retrieved."""
     for img in queryset:
-        img.queue_retrieve()
+        img.queue_retrieve_data()
 
 
 queue_retrieve.short_description = 'Queue retrieval of image data'
