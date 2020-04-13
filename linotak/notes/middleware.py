dc0388fe279217299d02a89e3fc8cbf65b86956a
Damian Cugley <pdc@alleged.org.uk>
2020-04-13 20:40:23 +0100

    Spelling of exception message.

--- linotak/notes/middleware.py
+++ linotak/notes/middleware.py
@@ -11,12 +11,12 @@ class SubdomainSeriesMiddleware:
         from django.conf import settings
 
         if not hasattr(settings, 'NOTES_DOMAIN'):
-            raise MiddlewareNotUsed('NOTES_DOMAIN not set so subdomain middlware not used')
+            raise MiddlewareNotUsed('NOTES_DOMAIN not set so subdomain middleware not used')
         domain = settings.NOTES_DOMAIN
         if not domain:
-            raise MiddlewareNotUsed('NOTES_DOMAIN empty so subdomain middlware not used')
+            raise MiddlewareNotUsed('NOTES_DOMAIN empty so subdomain middleware not used')
 
-        self.subdomain_re = re.compile(r'^([a-z0-9]{1,63})\.' + re.escape(domain) + '$')
+        self.subdomain_re = SubdomainSeriesMiddleware.regex_from_domain(domain)
         self.get_response = get_response
 
     def __call__(self, request):
@@ -28,3 +28,7 @@ class SubdomainSeriesMiddleware:
                 request.series_name = m.group(1)
 
         return self.get_response(request)
+
+    @classmethod
+    def regex_from_domain(cls, domain):
+        return re.compile(r'^([a-z0-9]{1,63})\.' + re.escape(domain) + '$')
