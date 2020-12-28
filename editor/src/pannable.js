bd9dcd9829bf86561f61eda2122400c86dd8bb17
Damian Cugley <pdc@alleged.org.uk>
2020-12-28 18:40:18 +0000

    Make pannable behaviour listen for touch events

--- editor/src/pannable.js
+++ editor/src/pannable.js
@@ -12,6 +12,7 @@
 export function pannable(node) {
     let x;
     let y;
+    const touches = []
 
     function handleMousedown(event) {
         x = event.clientX;
@@ -48,11 +49,99 @@ export function pannable(node) {
         window.removeEventListener('mouseup', handleMouseup);
     }
 
+
+    /**
+     * @param {TouchEvent} event 
+     */
+    function handleTouchStart(event) {
+        // TODO cancel existing panning 
+        for (const touch of event.changedTouches) {
+            touches.push(copyTouch(touch));
+        }
+        if (touches.length === 1) {
+            const { clientX, clientY } = touches[0];
+            x = clientX;
+            y = clientY;
+
+            node.dispatchEvent(new CustomEvent('panstart', { detail: { x, y } }));
+
+            window.addEventListener('touchmove', handleTouchMove);
+            window.addEventListener('touchend', handleTouchEnd);
+            window.addEventListener('touchcancel', handleTouchCancel);
+        }
+    }
+
+    function handleTouchMove(event) {
+        for (const touch of event.changedTouches) {
+            const i = findTouch(touch.identifier);
+            if (i >= 0) {
+                touches[i] = copyTouch(touch);
+            }
+        }
+        if (touches.length === 1) {
+            // TODO check in
+            const { clientX, clientY } = touches[0];
+            const dx = clientX - x;
+            const dy = clientY - y;
+            x = clientX;
+            y = clientY;
+            node.dispatchEvent(new CustomEvent('panmove', {
+                detail: { x, y, dx, dy }
+            }));
+        }
+    }
+
+    function handleTouchEnd(event) {
+        terminateTouches(event, 'panend');
+    }
+
+    function handleTouchCancel(event) {
+        terminateTouches(event, 'pancancel');
+    }
+
+    function terminateTouches(event, eventName) {
+        if (touches.length === 1 && event.changedTouches.length >= 1 && event.changedTouches[0].identifier === touches[0].identifier) {
+            const { clientX, clientY } = event.changedTouches[0];
+            x = clientX;
+            y = clientY;
+
+            node.dispatchEvent(new CustomEvent('pancancel', {
+                detail: { x, y }
+            }));
+
+            window.removeEventListener('touchmove', handleTouchMove);
+            window.removeEventListener('touchend', handleTouchEnd);
+            window.removeEventListener('touchcancel', handleTouchCancel);
+        }
+
+        for (const touch of event.changedTouches) {
+            const i = findTouch(touch.identifier);
+            if (i >= 0) {
+                touches.splice(i, 1);
+            }
+        }
+    }
+
+    function copyTouch({ clientX, clientY, identifier }) {
+        return { clientX, clientY, identifier };
+    }
+
+    function findTouch(identifier) {
+        for (let i = 0; i < touches.length; ++i) {
+            if (touches[i].identifier === identifier) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
     node.addEventListener('mousedown', handleMousedown);
+    node.addEventListener('touchstart', handleTouchStart);
 
     return {
         destroy() {
             node.removeEventListener('mousedown', handleMousedown);
+            node.removeEventListener('touchstart', handleTouchStart);
         }
     };
 }
